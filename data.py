4cc9d6d7fcb78acb7c4146ba4ad3a73b5bb419c0
CmdrApollo <henryj2545@gmail.com>
2025-06-13 22:53:57 -0500

    initial commit

---
+++ data.py
@@ -0,0 +1,129 @@
+class Buffer:
+    def __init__(self, width, height):
+        self.width, self.height = width, height
+        self._buf = [(' ', 0) for _ in range(self.width * self.height)]
+
+    def clear(self):
+        self._buf = [(' ', 0) for _ in range(self.width * self.height)]
+
+    def get(self, x, y):
+        try:
+            return self._buf[y * self.width + x]
+        except IndexError:
+            pass
+
+    def set_at(self, x, y, char, color):
+        try:
+            if x > -1 and y > -1 and x < self.width and y < self.height:
+                self._buf[y * self.width + x] = (char, color)
+        except IndexError:
+            pass
+    
+    def set_text(self, x, y, text, color):
+        i = j = 0
+        for char in text:
+            if char == '\n':
+                j += 1
+                i = 0
+                continue
+            self.set_at(x + i, y + j, char, color)
+            i += 1
+
+    def set_col(self, x, y, color):
+        char = self._buf[y * self.width + x][0]
+        self._buf[y * self.width + x] = (char, color)
+
+    def set_rect(self, x, y, w, h, color):
+        for i in range(w):
+            for j in range(h):
+                if (i, j) in [(0, 0), (w - 1, 0), (w - 1, h - 1), (0, h - 1)]:
+                    self.set_at(x + i, y + j, '+', color)
+                elif i == 0 or i == w - 1:
+                    self.set_at(x + i, y + j, '|', color)
+                elif j == 0 or j == h - 1:
+                    self.set_at(x + i, y + j, '-', color)
+
+    def set_rect_filled(self, x, y, w, h, char, color):
+        for i in range(w):
+            for j in range(h):
+                self.set_at(x + i, y + j, char, color)
+
+    def blit(self, other, x, y):
+        for i in range(other.width):
+            for j in range(other.height):
+                self.set_at(x + i, y + j, *other._buf[j * other.width + i])
+
+    def blit_level(self, active_vis, level, x, y, invisible_color=None):
+        for i in range(level.width):
+            for j in range(level.height):
+                char, color = level.buffer._buf[j * level.width + i]
+                if level.visibility[j * level.width + i] and not active_vis[j * level.width + i]:
+                    color = invisible_color
+
+                if level.visibility[j * level.width + i]:
+                    self.set_at(x + i, y + j, char, color)
+
+    def put(self, stdscr, x, y, colors):
+        for j in range(self.height):
+            for i in range(self.width):
+                c = self._buf[j * self.width + i]
+                stdscr.addch(y + j, x + i, c[0], c[1] if type(c[1]) != str else {
+                    'white': colors.WHITE,
+                    'red': colors.RED,
+                    'green': colors.GREEN,
+                    'blue': colors.BLUE,
+                    'yellow': colors.YELLOW,
+                    'cyan': colors.CYAN,
+                    'magenta': colors.MAGNETA,
+                    'inverse': colors.INVERSE,
+                }[c[1]])
+
+class Level:
+    def __init__(self, width, height):
+        self.width, self.height = width, height
+        self.buffer = Buffer(width, height)
+        self.visibility = [False for _ in range(width * height)]
+        self.entities = []
+    
+    def set_visibility(self, x, y, value):
+        self.visibility[y * self.width + x] = value
+    
+    def wallify(self, color):
+        for x in range(self.width):
+            for y in range(self.height):
+                for n in [(0, -1), (1, -1), (-1, -1), (0, 1), (1, 1), (-1, 1), (-1, 0), (1, 0)]:
+                    if 0 <= x + n[0] < self.width and 0 <= y + n[1] < self.height:
+                        if self.buffer.get(x + n[0], y + n[1])[0] == '.' and self.buffer.get(x, y)[0] == ' ':
+                            self.buffer.set_at(x, y, '#', color)
+                            break
+
+class Entity:
+    def __init__(self, x, y, name, description, char, color, solid=True):
+        self.x = x
+        self.y = y
+        self.name = name
+        self.description = description
+        self.char = char
+        self.color = color
+        self.solid = solid
+
+    def interact(self):
+        # default interaction
+        # tells what the player
+        # should do when it bump
+        # attacks this entity,
+        # in this case, show its
+        # description
+        return ("description", None)
+
+class NPC(Entity):
+    def __init__(self, x, y, name, description, dialogue, char, color):
+        super().__init__(x, y, name, description, char, color)
+        self.dialogue = dialogue
+    
+    def interact(self):
+        return ("dialogue", self.dialogue)
+
+class MangledKobold(NPC):
+    def __init__(self, x, y):
+        super().__init__(x, y, "Manged Kobold", "Before you, you see a Kobold who has been mangled by cybernetic enhancments; almost to the point of no recognition. You wonder if the poor creature is still whole beneath all of the technology that engulfs its small body. They grunt at you annoyedly.", "The kobold groans to life, machines sputtering as they do so. \"Go away,\" they tell you in a dry voice.", 'k', 'cyan')
---
+++ main.py
@@ -0,0 +1,205 @@
+import curses
+
+import tcod.path
+import tcod.map
+import tcod.constants
+
+from procgen import generate_dungeon
+
+from data import Buffer, Level
+
+import numpy as np
+
+GAMENAME = "Ethereal Collapse"
+
+end_text = "[ENTER] to continue"
+
+class GameScene:
+    MAIN_MENU = 0
+    PLAYING = 1
+
+def main(stdscr):
+    width, height = 80, 25
+
+    gameplay_width, gameplay_height = 60, 18
+
+    curses.start_color()
+
+    # Clear screen
+    curses.curs_set(0)  # Hide cursor
+    stdscr.nodelay(False)  # Wait for user input
+    stdscr.keypad(True)  # Enable arrow keys
+    stdscr.resize(height + 2, width + 2)
+
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(5, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(6, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+
+    class Colors:
+        WHITE = curses.color_pair(0)
+        RED = curses.color_pair(1)
+        GREEN = curses.color_pair(2)
+        BLUE = curses.color_pair(3)
+        YELLOW = curses.color_pair(4)
+        CYAN = curses.color_pair(5)
+        MAGNETA = curses.color_pair(6)
+        INVERSE = curses.color_pair(7)
+
+    def split_up(text, size_x=width, size_y=height):
+        final = [""]
+        for word in text.split():
+            if len(final[-1]) + len(word) + 1 >= size_x:
+                final.append("")
+            final[-1] += word + " "
+        return "\n".join(final[:size_y])
+
+    def text_box(x, y, w, h, title, contents, scr):
+        text = split_up(contents, w - 2, h) + f"\n{' ' * (w - len(end_text) - 2)}{end_text}"
+
+        scr.set_rect_filled(x, y, w, min(h, len(text.splitlines()) + 2), ' ', Colors.WHITE)
+        scr.set_rect(x, y, w, min(h, len(text.splitlines()) + 2), Colors.WHITE)
+        scr.set_text(x + w // 2 - len(title) // 2, y, title, Colors.WHITE)
+        scr.set_text(x + 1, y + 1, text, Colors.WHITE)
+
+    screen = Buffer(width, height)
+
+    def main_menu():
+        return True
+
+    def main_game():
+        game_screen = Buffer(gameplay_width - 2, gameplay_height - 2)
+
+        level, x, y = generate_dungeon(
+            15, 4, 6, width, height
+        )
+
+        level.wallify(Colors.WHITE)
+
+        active_visibility = level.visibility.copy()
+
+        solids = None
+        empty_solids = None
+
+        def generate_solids():
+            nonlocal solids
+            nonlocal empty_solids
+
+            # used for pathfinding and fov
+            solids = np.array([[ 1 for _ in range(level.width) ] for _ in range(level.height)])
+
+            empty_solids = solids.copy()
+
+            for x in range(level.width):
+                for y in range(level.height):
+                    if level.buffer.get(x, y)[0] == '#' or any([e.solid and e.x == x and e.y == y for e in level.entities]):
+                        solids[y, x] = 0
+
+        cam_x = x - gameplay_width // 2
+        cam_y = y - gameplay_height // 2
+
+        show_text = False
+
+        title = ""
+        contents = ""
+
+        while True:
+            generate_solids()
+            fov = tcod.map.compute_fov(solids, (y, x), 4, algorithm=tcod.constants.FOV_DIAMOND)
+            for j in range(level.height):
+                for i in range(level.width):
+                    if fov[j, i]:
+                        active_visibility[j * level.width + i] = True
+                        level.set_visibility(i, j, True)
+                    else:
+                        active_visibility[j * level.width + i] = False
+
+            stdscr.clear()
+            screen.clear()
+            game_screen.clear()
+
+            stdscr.subwin(height + 2, width + 2, 0, 0).box()
+            stdscr.addstr(0, 1, GAMENAME)
+
+            screen.set_rect(0, 0, gameplay_width, gameplay_height, Colors.WHITE)
+            screen.set_text(1, 0, "Game", Colors.WHITE)
+            screen.set_rect(gameplay_width, 0, width - gameplay_width, gameplay_height, Colors.WHITE)
+            screen.set_text(gameplay_width + 1, 0, "Inventory", Colors.WHITE)
+            screen.set_rect(0, gameplay_height, gameplay_width, height - gameplay_height, Colors.WHITE)
+            screen.set_text(1, gameplay_height, "Messages", Colors.WHITE)
+            screen.set_rect(gameplay_width, gameplay_height, width - gameplay_width, height - gameplay_height, Colors.WHITE)
+            screen.set_text(gameplay_width + 1, gameplay_height, "???", Colors.WHITE)
+
+            game_screen.blit_level(active_visibility, level, -cam_x, -cam_y, Colors.BLUE)
+
+            for entity in level.entities:
+                if active_visibility[entity.y * level.width + entity.x]:
+                    game_screen.set_at(entity.x - cam_x, entity.y - cam_y, entity.char, entity.color)
+
+            game_screen.set_at(x - cam_x, y - cam_y, '@', Colors.YELLOW)
+            
+            if show_text:
+                text_box(0, 0, game_screen.width, game_screen.height, title, contents, game_screen)
+
+            screen.blit(game_screen, 1, 1)
+
+            screen.put(stdscr, 1, 1, Colors)
+
+            stdscr.refresh()
+
+            # Get user input
+            key = stdscr.getch()
+
+            if show_text:
+                if key == ord('\n'):
+                    show_text = False
+            else:
+                ox, oy = x, y
+
+                if key == curses.KEY_UP:
+                    y = max(0, y - 1)
+                elif key == curses.KEY_DOWN:
+                    y = min(height - 1, y + 1)
+                elif key == curses.KEY_LEFT:
+                    x = max(0, x - 1)
+                elif key == curses.KEY_RIGHT:
+                    x = min(width - 1, x + 1)
+                elif key == ord('l'):
+                    for n in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+                        for entity in level.entities:
+                            if entity.x == x + n[0] and entity.y == y + n[1]:
+                                show_text = True
+                                title = entity.name
+                                contents = entity.description
+                                break
+                elif key == 27:
+                    break
+
+                if not solids[y, x]:
+                    for entity in level.entities:
+                        if entity.x == x and entity.y == y:
+                            interaction_type, interaction_data = entity.interact()
+
+                            match interaction_type:
+                                case "description":
+                                    show_text = True
+                                    title = entity.name
+                                    contents = entity.description
+                                case "dialogue":
+                                    show_text = True
+                                    title = entity.name
+                                    contents = interaction_data
+                    x, y = ox, oy
+
+            cam_x = x - gameplay_width // 2
+            cam_y = y - gameplay_height // 2
+
+    while True:
+        if not main_menu():
+            break
+        main_game()
+
+if __name__ == '__main__':
+    curses.wrapper(main)
